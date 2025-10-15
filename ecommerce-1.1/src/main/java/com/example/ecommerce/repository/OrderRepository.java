a5015d0ae1543ed1ef00d7b68bdbfb23a9ae7d34
CamilliFeliz <camilliffeliz@gmail.com>
2025-10-15 09:42:24 -0300

    Delete ecommerce-1.1 directory

--- ecommerce-1.1/src/main/java/com/example/ecommerce/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-
-package com.example.ecommerce.repository;
-
-import com.example.ecommerce.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
